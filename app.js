916209145b9373035cbe36f0af90b7a06c42bba9
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2025-07-22 21:03:39 +0500

    Data Base Login

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const supabaseUrl = 'https://ipoviueuhflhqjemgfkw.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb3ZpdWV1aGZsaHFqZW1nZmt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5Mzc5NzEsImV4cCI6MjA2ODUxMzk3MX0.ACNfp4MRa7-r1YYmu04VDltBo5UudrKWWw2NyDPBrk0'
 const supa = supabase.createClient(supabaseUrl, supabaseKey)
 
-
+let p = document.getElementById('p')
 
 async function signUp() {
     let email = document.getElementById('c-email').value
     let password = document.getElementById('c-pass').value
+    p.innerText = 'Loading...'
     const { data, error } = await supa.auth.signUp({
         email, password
     })
+    p.innerText = ''
     if (error) {
-        alert(error.massage)
+        alert(error.message)
     } else {
         alert('Please Verify Email')
-        window.Location.href = 'login.html'
+        location.href = `login.html`
     }
 }
 
@@ -22,13 +24,37 @@ async function signUp() {
 async function login() {
     let email = document.getElementById('email').value
     let password = document.getElementById('pass').value
+    p.innerText = 'Loading...'
     const { data, error } = await supa.auth.signInWithPassword({
         email, password
     })
+    p.innerText = ``
     if (error) {
         alert(error.massage)
     } else {
         alert('Sucessfuly Login')
         window.location.href = `dashboard.html`
+    }
+}
+
+
+
+async function google() {
+    p.innerText = 'Loading...'
+    const { data, error } = await supa.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            redirectTo: 'https://www.w3schools.com'
+        }
+    })
+    p.innerText = ''
+    if (error) {
+        alert(error.message)
+    } else {
+        alert('Sucessfuly Login')
+
+        console.log('data',data);
+        
+        // location.href = `dashboard.html`
     }
 }
