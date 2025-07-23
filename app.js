d1cdeda5353dd38f2226efadf883e7647e2016af
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2025-07-23 19:58:09 +0500

    Supa Base Login SignUp

--- app.js
+++ app.js
@@ -2,33 +2,42 @@ const supabaseUrl = 'https://ipoviueuhflhqjemgfkw.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwb3ZpdWV1aGZsaHFqZW1nZmt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5Mzc5NzEsImV4cCI6MjA2ODUxMzk3MX0.ACNfp4MRa7-r1YYmu04VDltBo5UudrKWWw2NyDPBrk0'
 const supa = supabase.createClient(supabaseUrl, supabaseKey)
 
-let p = document.getElementById('p')
-
-async function signUp() {
-    let email = document.getElementById('c-email').value
-    let password = document.getElementById('c-pass').value
-    p.innerText = 'Loading...'
-    const { data, error } = await supa.auth.signUp({
-        email, password
-    })
-    p.innerText = ''
-    if (error) {
-        alert(error.message)
-    } else {
-        alert('Please Verify Email')
-        window.location.href = `login.html`
+token();
+async function token() {
+    const { data, error } = await supa.auth.getSession();
+    if (data.session) {
+        window.location.href = `dashboard.html`;
     }
 }
 
 
-async function login() {
-    let email = document.getElementById('email').value
-    let password = document.getElementById('pass').value
-    p.innerText = 'Loading...'
-    const { data, error } = await supa.auth.signInWithPassword({
-        email, password
+let cemail = document.getElementById('c-email')
+let cpassword = document.getElementById('c-pass')
+
+async function signUp() {
+    document.getElementById("loader").style.display = "flex";
+    const { data, error } = await supa.auth.signUp({
+        email: cemail.value, password: cpassword.value
     })
-    p.innerText = ``
+    document.getElementById("loader").style.display = "none";
+
+    if (error) {
+        alert(error.message)
+    } else {
+        alert('Please Verify Email')
+        window.location.href = `index.html`
+    }
+}
+
+
+let email = document.getElementById('email')
+let password = document.getElementById('pass')
+async function login() {
+    document.getElementById("loader").style.display = "flex";
+    const { data, error } = await supa.auth.signInWithPassword({
+        email: email.value, password: password.value
+    })
+    document.getElementById("loader").style.display = "none";
     if (error) {
         alert(error.massage)
     } else {
@@ -42,19 +51,53 @@ async function login() {
 
 
 async function google() {
-    
+    document.getElementById("loader").style.display = "flex";
     const { data, error } = await supa.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: 'https://muhammadhasnain112.github.io/Supa-Base/dashboard.html'
+            redirectTo: 'https://supabase.netlify.app/dashboard.html'
         }
     })
+        document.getElementById("loader").style.display = "none";
     if (error) {
         alert(error.message)
     } else {
         // location.href = `dashboard.html`
-        console.log('data',data);
+        console.log('data', data);
         alert('Sucessfuly Login')
-        
+
+    }
+}
+
+
+
+
+let flag = true
+function visiblePass() {
+    // flag = true
+    let visible = document.getElementById('visible')
+    if (flag) {
+        visible.src = `img/visible.png`
+        cpassword.type = `text`
+        flag = false
+    } else {
+        visible.src = `img/invisible.png`
+        cpassword.type = `password`
+        flag = true
+    }
+}
+
+
+
+function SignUpvisiblePass() {
+    let visible = document.getElementById('visible')
+    if (flag) {
+        visible.src = `img/visible.png`
+        password.type = `text`
+        flag = false
+    } else {
+        visible.src = `img/invisible.png`
+        password.type = `password`
+        flag = true
     }
 }
