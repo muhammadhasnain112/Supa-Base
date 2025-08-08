43849e0c6f62c57d22462822fcfdd42e174ba755
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2025-08-08 18:51:47 +0500

    Supa Base Post App

--- app.js
+++ app.js
@@ -7,27 +7,75 @@ const supa = supabase.createClient(supabaseUrl, supabaseKey)
 let cemail = document.getElementById('c-email')
 let cpassword = document.getElementById('c-pass')
 let cname = document.getElementById('c-name')
+let cImage = document.getElementById('c-img')
 
 async function signUp() {
     document.getElementById("loader").style.display = "flex";
-    const { data, error } = await supa.auth.signUp({
+    const { data: signupdata, error } = await supa.auth.signUp({
         email: cemail.value, password: cpassword.value,
         options: {
-      data: {
-        display_name: cname.value,
-     
-      }
-    }
+            data: {
+                display_name: cname.value,
+            }
+        }
     })
     document.getElementById("loader").style.display = "none";
-
     if (error) {
         alert(error.message)
     } else {
-        alert('Please Verify Email')
-        window.location.href = `index.html`
+        const { error: abc } = await supa
+            .from('userInfo')
+            .insert({ email: cemail.value, name: cname.value })
+        const { data, error } = await supa
+            .from('userInfo')
+            .select('userid')
+            .eq('email', cemail.value)
+        if (error) {
+            console.log(error.message);
+
+        }
+        console.log(data[0].userid);
+        let Image = data[0].userid
+
+        const file = cImage.files[0];
+        if (!file) {
+            alert('Please select a file');
+            return;
+        }
+        const fileExt = file.name.split('.').pop(); // avif, jpg etc.
+        const fileName = `Login-User-Image/${Image}.${fileExt}`;
+        const { data: userImage, error: userError } = await supa.storage
+            .from('logedinprofile')
+            .upload(fileName, file, {
+            });
+        const { data: publicurl } = supa
+            .storage
+            .from('logedinprofile')
+            .getPublicUrl(fileName)
+        console.log(publicurl.publicUrl);
+
+        const { error: update } = await supa
+            .from('userInfo')
+            .update({ imgurl: publicurl.publicUrl })
+            .eq('email', cemail.value)
+        if (userError) {
+            console.error('Upload error:', userError.message);
+            alert('Upload failed: ' + userError.message);
+        } else {
+            console.log('Upload success:', userImage);
+            alert('Image uploaded successfully!');
+            alert('Please Verify Email')
+            window.location.href = `index.html`
+        }
     }
 }
+
+
+
+
+
+
+
 
 
 let email = document.getElementById('email')
